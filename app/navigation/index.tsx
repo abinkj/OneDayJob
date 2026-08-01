10040f0e1e15310ba115eab3c73d3369ca0c3e1e
AJ <ajayjosephstudy@gmail.com>
2026-08-01 20:10:41 +0530

    chore: comment out navigationBarHidden configuration in stack navigator options

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -57,7 +57,7 @@ const RootStackLayout = () => {
       screenOptions={{
         headerShown: false,
         animation: "slide_from_right",
-        navigationBarHidden:true
+        //navigationBarHidden:true
       }}
     >
       {isLoggedIn ? (
